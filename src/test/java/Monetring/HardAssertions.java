2fbc00d0b44a15594eb9377c5764cfc1e000109e
Lina Khaled <linaxn4@gmail.com>
2025-09-22 13:01:15 +0300

    Add TestNG configuration and example test classes for annotation and priority testing

--- src/test/java/Monetring/HardAssertions.java
+++ src/test/java/Monetring/HardAssertions.java
@@ -1,11 +1,17 @@
 package Monetring;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utilities.TestBase;
 
-public class HardAssertions extends TestBase {
+import java.time.Duration;
+
+public class HardAssertions  {
 //    Task 4: Hard Assertions Practice
 //    Objective: Implement hard assertions for login functionality
 //    Test Case: Positive Login Test
@@ -18,17 +24,28 @@ public class HardAssertions extends TestBase {
 //    New page URL is "
 //    https://claruswaysda.github.io/signIn.html"
 //    Page contains text "Employee Table
-
+protected WebDriver driver;
+@BeforeClass
+void beforeClass(){
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+}
     @Test
     void test() {
         driver.get("https://claruswaysda.github.io/signIn.html");
-        driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys("123");
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).sendKeys("123");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
 
         // Hard assertions
         Assert.assertEquals(driver.getCurrentUrl(), "https://claruswaysda.github.io/signIn.html", "URL does not match expected value.");
         Assert.assertTrue(driver.getPageSource().contains("Employee Table"), "Page does not contain expected text 'Employee Table'.");
 
     }
-}
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }}
+
+
